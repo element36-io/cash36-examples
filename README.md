1a9734b8f38c5ca0321e088e06fca845b04d9047
walter.strametz@gmail.com <walter.strametz@gmail.com>
2020-05-18 17:02:05 +0200

    integration fibree contract

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-
-
 Some examples how to: 
 
 - Use element36 to wire-transfer Euro and CHF to smart contracts
@@ -13,7 +11,7 @@ With elment36, these organizations can reach out to users without blockchain kno
 use the tradional payment network e.g. for funding purposes, without loosing the 
 safety and transparency of blockchain apps. See a list of Aragon Dapps [here](https://poweredby.aragon.org/).  
 
-Test [WALLET-FREE](https://examples.e36.io/src/aragon.html) transfers to Aragon. You may create own contracts or use any ERC20 contract instead. Source of the demo project [here](./src/aragon.html).
+*Try out and test* [WALLET-FREE](https://examples.e36.io/src/aragon.html) transfers to Aragon. You may create own contracts or use any ERC20 contract instead. Source of the demo project is [here](./src/aragon.html).
 
 ## Ethereum: contract initiates wire-transfer
 
--- src/walletfree.js
+++ src/walletfree.js
@@ -1,11 +1,12 @@
 
-var baseUrl= "http://localhost:8090/cash36/" //"http://demo.e36.io:3000/";
+var demoBaseUrl= "http://demo.e36.io:8090/cash36/" //"http://demo.e36.io:3000/";
+var prodBaseUrl= "https://cash36.io/cash36/" //"http://demo.e36.io:3000/";
 
 
-function getPaymentInfo() {
-
+function simulatePayment() {
     var xhr = new XMLHttpRequest();
-    var url = baseUrl+"exchange/buy/public/for";
+    var url = demoBaseUrl+"exchange/buy/public/for";
+    console.log("calling "+url);
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
@@ -15,12 +16,33 @@ function getPaymentInfo() {
         }
     };
     var data = JSON.stringify({
-        "amount": 4443,
+        "amount": 5,
+        "symbol": "EUR36",
+        "targetAddress": "0xb62d5cfe67b69f9bf045a2f7ef923a384d533154",  // https://rinkeby.aragon.org/?#/fibreetest/0xb62d5cfe67b69f9bf045a2f7ef923a384d533154/
+        "targetAddressType": "CONTRACT", 
+        "userIban": "CH93 0076 2011 6238 5295 7DE89 3704 0044 0532 0130 00" // this is used in dev/test to simulate payments. You need to create a user, use this number when onboarding  
+      });
+    xhr.send(data);
+
+}
+function getPaymentInfo() {
+
+    var xhr = new XMLHttpRequest();
+    var url = prodBaseUrl+"exchange/buy/public/for";
+    console.log("calling "+url);    
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            showPaymentInfo(json);
+        }
+    };
+    var data = JSON.stringify({
+        "amount": 5,
         "symbol": "CHF36",
         "targetAddress": "0xeB237523c412a56dde5b8eE0Bc2A6faBfC187afd",
         "targetAddressType": "CONTRACT", 
-        "userIban": "DE89 3704 0044 0532 0130 00" // this is used in dev/test to simulate payments. You need to create a user, use this number when onboarding  
-
       });
     xhr.send(data);
 
@@ -28,4 +50,9 @@ function getPaymentInfo() {
 
 function showPaymentInfo(serverInfo) {
     console.log(JSON.stringify(serverInfo));
+    for(var key in serverInfo)  {
+        if (document.getElementById(key)) {
+            document.getElementById(key).innerHTML=serverInfo[key];
+        }
+    };
 }
